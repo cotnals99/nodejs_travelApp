9c196bce39b2037bf9dc7c4939d004d8d47dec8b
cotnals <su.chae@icloud.com>
2022-04-29 12:38:15 +0700

    Fix the bounds error in finding local places

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     const [places, setPlaces] = useState();
 
     const [ coordinates, setCoordinates ] = useState({});
-    const [ bounds, setBounds ] = useState(null);
+    const [ bounds, setBounds ] = useState({ });
 
     useEffect(()=>{
         navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
@@ -25,7 +25,7 @@ const App = () => {
     useEffect(()=>{
         // console.log(coordinates, bounds);
 
-        getPlacesData()
+        getPlacesData(bounds.sw, bounds.ne)
             .then((data)=>{
                 setPlaces(data);
                 // console.log(data)
--- src/api/index.js
+++ src/api/index.js
@@ -4,22 +4,22 @@ import axios from "axios";
 
 const URL = 'https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary'
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    bl_longitude: '108.473209',
-    tr_longitude: '109.149359',
-    tr_latitude: '12.838442',
-    // limit: '30',
-    // currency: 'USD',
-    // subcategory: 'hotel,bb,specialty',
-    // adults: '1'
-  },
-  headers: {
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-    'X-RapidAPI-Key': '2a8d1e7f13msh38945db7e8c570ap1b7baajsnbb927d641e1a'
-  }
-};
+// const options = {
+//   params: {
+//     bl_latitude: '11.847676',
+//     bl_longitude: '108.473209',
+//     tr_longitude: '109.149359',
+//     tr_latitude: '12.838442',
+//     limit: '30',
+//     currency: 'USD',
+//     subcategory: 'hotel,bb,specialty',
+//     adults: '1'
+//   },
+//   headers: {
+//     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+//     'X-RapidAPI-Key': '2a8d1e7f13msh38945db7e8c570ap1b7baajsnbb927d641e1a'
+//   }
+// };
 
 // axios.request(options).then(function (response) {
 // 	console.log(response.data);
@@ -30,8 +30,23 @@ const options = {
 export const getPlacesData = async (sw, ne) => {
     try {
         //request
-        const {data: {data}} = await axios.get(URL, options);
-        // console.log(data)
+        const {data: {data}} = await axios.get(URL, {
+          params: {
+            bl_latitude: sw.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+            tr_latitude: ne.lat,
+            // limit: '30',
+            // currency: 'USD',
+            // subcategory: 'hotel,bb,specialty',
+            // adults: '1'
+          },
+          headers: {
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+            'X-RapidAPI-Key': '2a8d1e7f13msh38945db7e8c570ap1b7baajsnbb927d641e1a'
+          }
+        });
+        console.log(data)
         return data;
 
         // const response = await axios.get(URL, options);
